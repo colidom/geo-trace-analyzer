1d7d4fb27a429f88ebcc37fe24d59d717d469edf
colidom <colidom@outlook.com>
2025-01-07 02:18:50 +0000

    Refactor and enhance documentation for Map and FileSystem.
    Updated method docstrings across `Map` and `FileSystem` classes to improve clarity, consistency, and expand explanations of arguments, returns, and functionality. Improved readability of code and ensured adherence to best practices in comments and documentation style.

--- classes/FileSystem.py
+++ classes/FileSystem.py
@@ -5,15 +5,40 @@ from dotenv import load_dotenv
 
 
 class FileSystem:
-    """Clase para manejar operaciones relacionadas con el sistema de archivos y configuraciones."""
-
     def __init__(self, base_dir=None):
-        """Inicializa el FileSystem con un directorio base opcional."""
+        """
+        Initializes an instance of the class with a base directory.
+
+        If no base directory is specified, the current working
+        directory is used as the default base directory.
+
+        Args:
+            base_dir (Optional[str]): The base directory for the instance. If
+            None, defaults to the current working directory.
+        """
         self.base_dir = base_dir or os.getcwd()
 
     @staticmethod
     def get_csv_file(data_path):
-        """Detecta automáticamente el único archivo CSV en una carpeta o valida una ruta específica de archivo CSV."""
+        """
+            Retrieves the path to a CSV file from a given directory or validates a specified file.
+
+            If the provided path corresponds to a directory, it attempts to locate a single CSV
+            file within that directory. If the path is a file, it checks if it has a ".csv"
+            extension. If any of these conditions are not met or if the file/directory does not
+            exist, corresponding errors are raised.
+
+            Raises:
+                ValueError: If the provided file is not a CSV or if the directory contains
+                zero or multiple CSV files.
+                FileNotFoundError: If the provided path does not exist.
+
+            Args:
+                data_path: Path to a file or directory to validate or locate a CSV file.
+
+            Returns:
+                str: Path to the located or validated CSV file.
+        """
         if os.path.isfile(data_path):
             # Si es un archivo, verificamos que sea un CSV
             if data_path.endswith(".csv"):
@@ -34,8 +59,17 @@ class FileSystem:
     @staticmethod
     def read_data(csv_file):
         """
-        Lee un archivo CSV y devuelve un DataFrame con coordenadas filtradas
-        basándose en el valor de precisión definido en las configuraciones.
+        Reads data from a specified CSV file, filters rows based on a precision
+        threshold, and returns the filtered DataFrame.
+
+        Parameters:
+        csv_file: str
+            Path to the CSV file to be read.
+
+        Returns:
+        pandas.DataFrame
+            A DataFrame containing rows from the CSV file filtered based on the
+            precision threshold.
         """
         _, _, valid_precision = FileSystem.load_configuration()
         df = pd.read_csv(csv_file)
@@ -43,19 +77,51 @@ class FileSystem:
 
     @staticmethod
     def create_directories(directories):
-        """Crea múltiples directorios si no existen."""
+        """
+            Creates multiple directories if they do not already exist.
+
+            This method iterates through a list of directory paths and ensures that
+            each directory in the list is created. If the directory already exists,
+            no error will be raised due to the use of the `exist_ok=True` parameter.
+
+            Args:
+                directories: List of paths of directories to be created as strings.
+        """
         for directory in directories:
             os.makedirs(directory, exist_ok=True)
 
     @staticmethod
     def process_secured_areas(obj, secured_areas, prox_distance):
-        """Procesa las zonas protegidas."""
+        """
+            Processes secured areas and applies them as safe zones for the given object.
+
+            This function iterates through a list of secured areas and applies each area as
+            a safe zone to the provided object using the add_safe_zone method. It does so by
+            considering a specified proximity distance.
+
+            Arguments:
+                obj: The object for which the safe zones will be applied. It is expected to
+                have an add_safe_zone method.
+                secured_areas (list): A list of secured areas to be processed.
+                prox_distance (float): The proximity distance to be used for defining
+                the safe zones.
+        """
         for area in secured_areas:
             obj.add_safe_zone(area, prox_distance)
 
     @staticmethod
     def load_configuration():
-        """Carga las configuraciones necesarias desde las variables de entorno."""
+        """
+        Fetches configuration values from environment variables and processes them into
+        appropriate Python datatypes for later usage in the application. This includes
+        loading the .env file, extracting proximity distance, secured areas, and valid
+        precision, and converting them into usable forms.
+
+        @return: A tuple containing the following in order:
+                 - proximity_distance (int): The distance value used for proximity calculations.
+                 - secured_areas (list): A list of secured area details.
+                 - valid_precision (list): A list describing valid precision values.
+        """
         load_dotenv()
         proximity_distance = int(os.getenv("PROXIMITY_DISTANCE"))
         secured_areas = json.loads(os.getenv("SECURED_AREAS"))
@@ -63,13 +129,33 @@ class FileSystem:
         return proximity_distance, secured_areas, valid_precision
 
     def get_directories(self):
-        """Devuelve las rutas de los directorios de datos y resultados."""
+        """
+        Retrieves the 'data' and 'result' directories based on the base directory.
+
+        This function constructs paths for 'data' and 'result' directories using the
+        'base_dir' attribute of the class instance. These directories are assumed to
+        exist within the base directory. The function returns the full paths to both
+        directories as a tuple.
+
+        Returns:
+            tuple: A tuple containing two strings. The first string is the path to the
+            'data' directory, and the second string is the path to the 'result'
+            directory.
+        """
         data_dir = os.path.join(self.base_dir, "data")
         result_dir = os.path.join(self.base_dir, "result")
         return data_dir, result_dir
 
     def setup_environment(self) -> tuple[str, str, str]:
-        """Configura el entorno de trabajo."""
+        """
+        Sets up the environment by changing the working directory and creating
+        necessary directories. This method ensures that the base directory is
+        set, and relevant subdirectories for data and results are created.
+
+        Returns:
+            tuple[str, str, str]: A tuple containing the base directory, data
+            directory, and result directory paths, respectively.
+        """
         os.chdir(self.base_dir)
         data_dir, result_dir = self.get_directories()
         FileSystem.create_directories([data_dir, result_dir])
--- classes/Map.py
+++ classes/Map.py
@@ -5,11 +5,33 @@ from utils.distance import calculate_distance, extract_coordinates
 
 class Map:
     def __init__(self, center, zoom_start=15):
-        """Inicializa el mapa centrado en una ubicación específica."""
+        """
+        Initialize a Map object using the Folium library. The map is centered on the given coordinates with an initial zoom level.
+
+        Args:
+            center (tuple[float, float]): Latitude and longitude values representing the center of the map.
+            zoom_start (int, optional): The initial zoom level for the map. Defaults to 15.
+        """
         self.map = folium.Map(location=center, zoom_start=zoom_start)
 
     def add_safe_zone(self, secured_area, proximity_distance):
-        """Agrega un marcador y un círculo de proximidad para la zona segura con información detallada."""
+        """
+        Adds a safe zone to the map based on the given secured area details and proximity
+        distance. The method places a marker on the map representing the safe zone, adds a
+        descriptive tooltip, and draws a circle around the area to indicate the proximity
+        radius.
+
+        Args:
+            secured_area (Dict[str, Union[str, Tuple[float, float]]]): A dictionary
+                containing details of the secured area. This includes its name
+                (str), coordinates (tuple of latitude and longitude), and type
+                (str) of the area (used for icon representation).
+            proximity_distance (float): The distance specifying the proximity radius
+                around the secured area.
+
+        Returns:
+            None
+        """
         name = secured_area["name"]
         lat, lng = secured_area["coordinates"]
         icon_type = secured_area["type"]
@@ -22,7 +44,23 @@ class Map:
         )
 
     def add_proximity_circle(self, location, proximity_distance, color, tooltip):
-        """Crea un círculo de proximidad alrededor de una ubicación."""
+        """
+        Adds a proximity circle to the folium map.
+
+        This method creates a circle on the map at a specified location with a given
+        proximity distance (radius). The circle is styled with the provided color,
+        filled with a semi-transparent overlay, and includes a tooltip.
+
+        Args:
+            location (list[float]): Latitude and longitude coordinates of the circle's
+                center as a [latitude, longitude] list.
+            proximity_distance (float): Radius in meters for the proximity circle.
+            color (str): Border color of the circle in any valid CSS color format.
+            tooltip (str): Tooltip text to display on hover.
+
+        Returns:
+            None
+        """
         folium.Circle(
             location=location,
             radius=proximity_distance,
@@ -33,7 +71,19 @@ class Map:
         ).add_to(self.map)
 
     def add_marker(self, location, tooltip, color, icon):
-        """Agrega un marcador con información y un icono."""
+        """
+        Adds a marker to the map at a given location with a tooltip, icon, and color.
+
+        Parameters:
+        location: list
+            The latitude and longitude coordinates for the marker.
+        tooltip: str
+            The text to be displayed when hovering over the marker.
+        color: str
+            The color of the marker icon.
+        icon: str
+            The icon for the marker represented by a Font Awesome icon name.
+        """
         folium.Marker(
             location=location,
             tooltip=tooltip,
@@ -41,21 +91,66 @@ class Map:
         ).add_to(self.map)
 
     def add_aggressor_route(self, aggressor_positions, color):
-        """Añade el recorrido de los agresores como una línea poligonal al mapa."""
+        """
+        Adds a route to the map representing the aggressor's movement. This method
+        visualizes a path using a polyline if multiple positions of the aggressor
+        are provided. The route is drawn with the specified color.
+
+        Parameters:
+            aggressor_positions (list): A list of positions representing the
+            coordinates of the aggressor's path. Each position is expected to be a
+            tuple or list of latitude and longitude values.
+            color (str): The color of the polyline representing the route on the map.
+
+        Returns:
+            None
+        """
         if len(aggressor_positions) > 1:
             folium.PolyLine(
                 aggressor_positions, color=color, weight=2.5, opacity=1
             ).add_to(self.map)
 
     def save(self, result_folder, output_file):
-        """Guarda el mapa en un archivo HTML."""
+        """
+        Saves the current map instance to a specified folder and file.
+
+        This function ensures the target directory exists. If it does not,
+        the directory is created. Then the map instance linked to the 'self.map'
+        attribute is saved at the specified location using the output file name.
+        Finally, a success message is printed to confirm the operation.
+
+        Parameters:
+        result_folder : str
+            The directory where the map file will be saved. It can be an
+            absolute or relative path.
+        output_file : str
+            The name of the file where the map will be saved, including
+            its extension.
+
+        Raises:
+        None
+        """
         os.makedirs(result_folder, exist_ok=True)
         self.map.save(os.path.join(result_folder, output_file))
         print(f"Mapa generado exitosamente: {os.path.join(result_folder, output_file)}")
 
     @staticmethod
     def verify_location(location):
-        """Verifica y procesa una ubicación en diferentes formatos."""
+        """
+        Verify the format of a location and parses it into latitude and longitude.
+
+        This method takes a location input, validates its type, and converts it
+        into a tuple containing latitude and longitude values. It supports input
+        given as a comma-separated string or other formats. Non-supported types or
+        unexpected values trigger specific informational messages.
+
+        Args:
+            location (str | float): The location input to be verified and parsed.
+
+        Returns:
+            tuple[float, float] | None: A tuple containing latitude and longitude
+                as floats if the input is valid and parsable, otherwise None.
+        """
         if isinstance(location, str):
             lat, lng = map(float, location.split(","))
             return lat, lng
@@ -66,7 +161,29 @@ class Map:
 
     @staticmethod
     def add_tooltip(position=None, lng=None, lat=None, row=None, name=None):
-        """Crea un tooltip para incrustarlo en el mapa con la información requerida"""
+        """
+            Adds a tooltip with given information to be displayed in a specific format.
+
+            This method constructs an HTML-styled tooltip string containing details such as
+            name, coordinates (longitude and latitude), a position description, and any
+            additional data extracted from a provided row object. The tooltip format will
+            adapt based on the inputs provided.
+
+            Args:
+                position (str or None): Description of the location or a string
+                    identifier for the position. Defaults to None.
+                lng (float or None): Longitude coordinate. Defaults to None.
+                lat (float or None): Latitude coordinate. Defaults to None.
+                row (dict or None): Additional data to include in the tooltip, such as
+                    precision and time. Defaults to None.
+                name (str or None): Name or title to include in the tooltip. Defaults to None.
+
+            Returns:
+                str: A string representing the HTML-styled tooltip.
+
+            Raises:
+                (No specific errors raised by this method)
+        """
 
         tooltip = f"<center>{name}</center>"
 
@@ -91,8 +208,20 @@ class Map:
 
     def check_prox_and_add_markers(self, victim_data, aggressor_data, proximity_distance):
         """
-        Verifica la proximidad de las víctimas con los agresores y añade marcadores al mapa.
-        También dibuja el recorrido de los agresores.
+        Processes victim and aggressor data to determine proximity and add markers.
+
+        This function iterates over victim data rows, calculates proximity to aggressors,
+        and processes the coordinates of any detected victim within the specified proximity.
+        It increments the victim position for each processed row. After handling victims, it
+        processes the aggressor data to determine positions and adds a route marker for them.
+
+        Arguments:
+            victim_data: DataFrame containing victim information
+            aggressor_data: DataFrame containing aggressor information
+            proximity_distance (float): Maximum allowable distance to determine proximity
+
+        Returns:
+            None
         """
         victim_position = aggressor_position = 1
 
@@ -107,6 +236,30 @@ class Map:
         self.add_aggressor_route(aggressor_positions, "red")
 
     def process_victim(self, victim_data_row, aggressor_data, proximity_distance, victim_position):
+        """
+        Processes a victim entity by analyzing its coordinates, checking for proximity
+        to an aggressor, and taking necessary actions when proximity is confirmed.
+        Returns the victim's coordinates if processing is conducted.
+
+        Args:
+            victim_data_row: dict
+                A dictionary containing the data row for the victim, from which
+                location information can be extracted.
+            aggressor_data: Any
+                The data representing the aggressor, used to determine proximity.
+            proximity_distance: float
+                The distance threshold to determine whether an aggressor is near a victim.
+            victim_position: Any
+                Additional data or metadata representing a victim's position.
+
+        Returns:
+            tuple[float, float] or None:
+                Returns a tuple of latitude and longitude as the victim's coordinates if
+                they are processed, or None if coordinates are not available.
+
+        Raises:
+            Does not explicitly raise any exceptions.
+        """
         victim_coordinates = self.get_coordinates(victim_data_row, "location")
         if not victim_coordinates:
             return None
@@ -116,6 +269,21 @@ class Map:
         return victim_coordinates
 
     def process_aggressors(self, aggressor_data, aggressor_position):
+        """
+        Processes the aggressor data to extract coordinates, process entities, and maintain
+        their positions. This function iterates through aggressor data, retrieves geographical
+        coordinates, appends them to a list, and invokes additional processing methods
+        for each aggressor.
+
+        Args:
+            aggressor_data (pd.DataFrame): The data containing information about aggressors.
+            aggressor_position (int): The initial position index for aggressors.
+
+        Returns:
+            list: A list of tuples where each tuple represents the latitude and longitude
+            coordinates of an aggressor.
+
+        """
         aggressor_positions = []
         for _, aggressor_row in aggressor_data.iterrows():
             aggressor_coordinates = self.get_coordinates(aggressor_row, "location")
@@ -128,6 +296,21 @@ class Map:
         return aggressor_positions
 
     def is_aggressor_near(self, victim_lat, victim_lng, aggressor_data, proximity_distance):
+        """
+        Checks if there is an aggressor near the specified victim based on their
+        coordinates and a specified proximity distance. If an aggressor is found
+        within the proximity range, a proximity alert is triggered and the function
+        returns True.
+
+        Args:
+            victim_lat (float): Latitude of the victim's location.
+            victim_lng (float): Longitude of the victim's location.
+            aggressor_data (pandas.DataFrame): Data containing aggressor details.
+            proximity_distance (float): Distance in meters to define proximity range.
+
+        Returns:
+            bool: True if an aggressor is within the proximity distance, otherwise False.
+        """
         aggressor_nearby = False
         for _, aggressor_row in aggressor_data.iterrows():
             aggressor_coordinates = extract_coordinates(
@@ -146,11 +329,44 @@ class Map:
         return aggressor_nearby
 
     def process_entity(self, lat, lng, position, data_row, entity_type, color, icon):
+        """
+        Processes an entity by creating a tooltip and adding a marker to a map.
+
+        This function generates a tooltip using the provided entity information, then
+        places a marker on the map at the specified latitude and longitude. The marker
+        is styled with the provided color and icon.
+
+        Args:
+            lat: Latitude of the entity's location.
+            lng: Longitude of the entity's location.
+            position: A string representing the general position or area of the entity.
+            data_row: The dataset row containing additional information about the entity.
+            entity_type: The type or category of the entity.
+            color: The color to use for the marker representation.
+            icon: The icon to use for the marker representation.
+        """
         tooltip_text = self.add_tooltip(position, lng, lat, data_row, entity_type)
         self.add_marker((lat, lng), tooltip_text, color, icon)
 
     @staticmethod
     def get_coordinates(data_row, location_column):
+        """
+            Extracts coordinates from a data row using the specified location column.
+
+            This static method attempts to process and extract coordinates from a given
+            row of data by utilizing the location column provided. If an exception
+            occurs during processing, it will print an error message and return None.
+
+            Args:
+                data_row: The data row containing relevant information for extracting
+                          coordinates.
+                location_column: The name or index of the column in the data row from
+                                 which to extract the coordinates.
+
+            Returns:
+                A data object containing the extracted coordinates, or None if an
+                exception occurred during processing.
+        """
         try:
             return extract_coordinates(data_row, location_column)
         except Exception as e:

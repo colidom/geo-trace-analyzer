b41fae015f4a7c97bd1a35fe89a631e6b27759c8
Carlos Oliva <colidom@outlook.com>
2025-03-10 13:26:14 +0100

    Añadida mejora para centrar el mapa en el area segura que queramos o en ninguna de ellas y en ese caso se hará en la primera coordenada del agresor

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ from classes.Map import Map
 
 
 def main():
-    # Instanciar FileSystem y configurar el entorno
     fs = FileSystem()
+    
+    # Cargar la configuración
     proximity_distance, secured_areas, _ = FileSystem.load_configuration()
+    
+    # Configurar el entorno de trabajo
     _, data_dir, result_dir = fs.setup_environment()
 
-    # Configuración del mapa
-    map_center = secured_areas[0]["coordinates"]
+    # Buscar la primera área segura activa y usarla para centrar el mapa
+    active_area = next((area for area in secured_areas if area['active']), None)
+
+    if active_area:
+        map_center = active_area["coordinates"]
+    else:
+        # Centrar en el primer agresor si no hay área activa
+        aggressor_data = FileSystem.read_data(FileSystem.get_csv_file(os.path.join(data_dir, "A.csv")))
+        first_aggressor_location = aggressor_data.iloc[0]['location'].split(',')
+        map_center = [float(first_aggressor_location[0]), float(first_aggressor_location[1])]
+
+    # Crear el mapa
     map_instance = Map(map_center)
 
-    # Carga de datos
+    # Cargar los datos de agresores y víctimas
     aggressor_data = FileSystem.read_data(FileSystem.get_csv_file(os.path.join(data_dir, "A.csv")))
     victim_data = FileSystem.read_data(FileSystem.get_csv_file(os.path.join(data_dir, "V.csv")))
 
